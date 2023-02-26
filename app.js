a13c593428008bea3540d1e432be4ef28dc105a3
dev-rashed-alam <dev.rashedalam@gmail.com>
2023-02-26 21:14:12 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const mongoose = require("mongoose");
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
+const loginRouter = require("./routers/loginRouter");
+
+const app = express();
+dotenv.config();
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_CONNECTION_URL)
+  .then(() => {
+    console.log("DB connection successful!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", loginRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
+});
