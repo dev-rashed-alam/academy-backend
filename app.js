d22d18edc4bb940b42899d5533768969120c73e5
dev-rashed-alam <dev.rashedalam@gmail.com>
2023-10-14 16:39:54 +0600

    Remove academy initial segment from route

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/academy", academyRouter);
+app.use("/", academyRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
